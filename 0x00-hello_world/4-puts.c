64a329a25041fbcc11d7b3ab9a421552504a8537
samutachika mushibi <samutachika@gmail.com>
2023-02-19 11:33:14 -0800

    Hello, Pust

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,11 @@
  * Main: takes no input is void and  puts  string
  * Return: return zero 0 interger
  **/
+
+/* take no agumnets put and returns zero*/
 int main(void)
 {
-	/* return int*/
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
