1133fe8d9e734065ce7daabcecce7d009d08ffbb
samutachika mushibi <samutachika@gmail.com>
2023-02-22 06:19:41 -0800

    printing putcharacter

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,14 @@
  */
 int main(void)
 {
-_putchar('_ \n');
-_putchar('p \n');
-_putchar('u \n');
-_putchar('t\n');
-_putchar('c \n');
-_putchar('h \n');
-_putchar('a \n');
-_putchar('r \n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
