385c556bbd033236625ebb440ac08a6e8065561e
samutachika mushibi <samutachika@gmail.com>
2023-02-22 06:11:59 -0800

    printing putcharacter

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - prints _putchar
+ * Return: returns zero
+ */
+int main(void)
+{
+_putchar('_ \n');
+_putchar('p \n');
+_putchar('u \n');
+_putchar('t\n');
+_putchar('c \n');
+_putchar('h \n');
+_putchar('a \n');
+_putchar('r \n');
+return (0);
+}
