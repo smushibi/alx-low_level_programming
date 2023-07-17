95f559cee92ef687729a399ea468dbf80917b86b
smushibi <samutachika@gmail.com>
2023-07-17 16:57:49 -0700

    Prints the numbers 0 to 9 separated by commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program
- * Description: Prints the numbers 0 to 9 separated by commas
+ * Description: Prints the numbers 0 to 9 separated by spaces
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int digits[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 int i;
 for (i = 0; i < 10; i++)
 {
-putchar(digits[i] + '0');
-if (i != 9)
-putchar(',');
+putchar(digits[i]);
+if (i % 3 == 0)
+{
 putchar(' ');
+}
 }
 return (0);
 }
