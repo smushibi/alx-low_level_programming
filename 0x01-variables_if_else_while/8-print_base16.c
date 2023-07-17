20a2aca7ad57e85af633dc6c1efd0a59224341b3
smushibi <samutachika@gmail.com>
2023-07-17 16:10:43 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description: Prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[] = "0123456789abcdef";
+for (int i = 0; i < 16 ; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
