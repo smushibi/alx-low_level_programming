d698eab28867c1019968d63864dc517f2f646787
smushibi <samutachika@gmail.com>
2023-07-19 11:59:00 -0700

    must leran ACII

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,10 @@
-#include <ctype.h>
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
- *
- *
  */
 int _isalpha(int c)
 {
-static const int is_alpha_lookup[256] = {
-[0 ... 31] = 0,
-[32 ... 47] = 0,
-[48 ... 57] = 0,
-[58 ... 64] = 0,
-[65 ... 90] = 1,
-[91 ... 96] = 0,
-[97 ... 122] = 1,
-[123 ... 255] = 0
-};
-return (is_alpha_lookup[(unsigned char)c]);
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
