5f864e39f91a06d4fb2d64815e0113e4eb039993
smushibi <samutachika@gmail.com>
2023-07-18 23:10:32 -0700

    print alp in low

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,12 @@
 /**
  * print_alphabet - Prints all alphabets in lowercase.
  * Description: This function prints all the lowercase alphabets from 'a' to 'z'.
+ * Each alphabet is printed using the _putchar function.
  */
 void print_alphabet(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter);
-putchar('\n');
+_putchar(letter);
+_putchar('\n');
 }
