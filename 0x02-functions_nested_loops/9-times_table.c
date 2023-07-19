eca2e317c397a1bb9f3ead0e8850ebe462cf5d51
smushibi <samutachika@gmail.com>
2023-07-19 12:39:41 -0700

    9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * times_table - check the code
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+for (int i = 1; i <= 9; i++)
+{
+for (int j = 1; j <= 9; j++)
+{
+printf("%d * %d = %d\n", i, j, i * j);
+}
+}
+return (0);
+}
