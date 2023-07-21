21389b7a7c4f9d15c6144e9b5062e25dee8c4883
smushibi <samutachika@gmail.com>
2023-07-21 11:17:59 -0700

    checks digit

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -2,17 +2,6 @@
 #include <stdio.h>
 
 /**
- * _isupper - checks for uppercase character
- * @c: The character to be checked
- *
- * Return: 1 if c is uppercase, 0 otherwise
- */
-int _isupper(int c)
-{
-    return (c >= 'A' && c <= 'Z') ? 1 : 0;
-}
-
-/**
  * main - check the code
  *
  * Return: Always 0.
@@ -21,9 +10,9 @@ int main(void)
 {
     char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
     c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return 0;
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
 }
