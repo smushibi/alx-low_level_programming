2ca1b0f126809cef64811334afea27071498d26f
smushibi <samutachika@gmail.com>
2023-07-21 17:30:26 -0700

    also 0-9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #ifndef MAN_H
 #define MAN_H
 
-
+int _putchar(char c)
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-int _putchar(char c)
+
 #endif
