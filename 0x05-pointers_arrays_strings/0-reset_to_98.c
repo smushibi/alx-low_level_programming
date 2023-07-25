a9e2fb2798d1f4ef47a9a12bf1a4111f7bda372e
smushibi <samutachika@gmail.com>
2023-07-25 08:23:53 -0700

    pinter in action

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/*
+ * reset_to_98 - Takes a pointer to an int as a parameter and updates the value
+ *               it points to 98.
+ * @n: A pointer to an integer.
+ */
+void reset_to_98(int *n) {
+*n = 98;
+}
