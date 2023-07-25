40e1a3ef73b4be86566232c48c2c8396d14b9e0e
smushibi <samutachika@gmail.com>
2023-07-25 13:25:53 -0700

    keygan redo sepio

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Generates a random password and prints it.
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+char password[10];
+srand(time(NULL));
+for (int i = 0; i < 10; i++)
+{
+password[i] = rand() % 128;
+}
+printf("%s\n", password);
+return (0);
+}
