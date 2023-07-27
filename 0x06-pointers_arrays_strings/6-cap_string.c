dcc7e421c05a418f43ee99656a123025b5740e9e
smushibi <samutachika@gmail.com>
2023-07-27 06:51:45 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,17 +6,24 @@
  *
  * Return: A pointer to the changed string.
  */
+
 char *cap_string(char *str)
 {
 int index = 0;
+int is_word_start = 1;
 while (str[index])
 {
-if (isalpha(str[index]))
+if (isalpha(str[index]) || str[index] == '_')
 {
-if (index == 0 || !isalpha(str[index - 1]))
+if (is_word_start)
 {
 str[index] -= 32;
 }
+is_word_start = 0;
+}
+else
+{
+is_word_start = 1;
 }
 index++;
 }
