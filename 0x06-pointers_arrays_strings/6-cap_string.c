75d5d19d1fe23784dc2be75258a820450c813207
smushibi <samutachika@gmail.com>
2023-07-27 06:41:46 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,5 @@
 #include <ctype.h>
 #include "main.h"
-
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
@@ -10,17 +9,14 @@
 char *cap_string(char *str)
 {
 int index = 0;
-int capitalize_next = 1;
 while (str[index])
 {
-if (capitalize_next && isalpha(str[index]))
+if (isalpha(str[index]))
 {
-str[index] = toupper(str[index]);
-capitalize_next = 0;
+if (index == 0 || !isalpha(str[index - 1]))
+{
+str[index] -= 32;
 }
-else if (isspace(str[index]) || ispunct(str[index]))
-{
-capitalize_next = 1;
 }
 index++;
 }
