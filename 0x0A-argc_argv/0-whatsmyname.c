d18da7bac6dea730cbe7383b318b65af7e408ff6
smushibi <samutachika@gmail.com>
2023-08-07 11:33:28 -0700

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+/*
+ * main - Entry point for program
+ * Decription: program that prints its name,
+ * followed by a new line.
+ * @argc - argument conter
+ * @argv - argument array
+ */
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
