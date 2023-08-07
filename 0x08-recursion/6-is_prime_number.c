fc33da93ef532566d61700f98bfb069da200fef5
smushibi <samutachika@gmail.com>
2023-08-07 05:33:55 -0700

    libs in c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,15 +6,20 @@
  * @i: The divisor to check for divisibility.
  *
  * Return: 1 if the number is prime, otherwise return 0.
- */
-int is_prime_helper(int n, int i);
-
-/**
- * is_prime_number - Returns 1 if the input integer is a prime number,
- * otherwise return 0.
+ *
+ * is_prime_number - Check if the input integer is a prime number.
  * @n: The number to be checked.
- * Return: 1 if the number is prime, otherwise return 0.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ *
+ * Description: This function checks whether the input integer 'n'
+ * is a prime number.
+ * It returns 1 if 'n' is prime, and 0 otherwise. A number is
+ *  prime if it
+ * is greater than 1 and has no divisors other than 1 and itself.
  */
+
+int is_prime_helper(int n, int i);
 int is_prime_number(int n)
 {
 if (n <= 1)
