1defc76c255a3ac9c74422df9236a637840a0bf9
smushibi <samutachika@gmail.com>
2023-08-08 00:27:40 -0700

    argv in action

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
-/*
- * main - Entry point for program
- * Decription: program that prints its name,
- * followed by a new line.
- * @argc - argument conter
- * @argv - argument array
- */
-
 #include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of pointers to the command-line arguments
+ *
+ * Return: 0 (Always success)
+ */
 int main(int argc, char *argv[])
 {
+(void)argc;
 printf("%s\n", argv[0]);
 return (0);
 }
+
