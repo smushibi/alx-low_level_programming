1ffcf2e4caac2ec672ab14690503a47ccb58f1fe
smushibi <samutachika@gmail.com>
2023-08-14 13:37:01 -0700

    the dog header

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -9,9 +9,9 @@
 int main(void)
 {
     struct dog my_dog = {
-        "Poppy",     // Name
-        3.5,         // Age
-        "Bob"        // Owner
+        "Poppy",     
+        3.5,         
+        "Bob"        
     };
 
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
