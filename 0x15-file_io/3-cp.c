6422bddab552b3228a84f1ac22463dc7d37fd990
smushibi <samutachika@gmail.com>
2023-09-08 14:12:05 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
 
@@ -10,52 +12,46 @@
  * @argc: The number of command line arguments
  * @argv: An array containing the command line arguments
  *
- * Return: 0 on success, otherwise an error code
+ * Return: (0) on success, otherwise an error code
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			break;
-	}
-
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-		exit(100);
-	}
-
-	return (0);
+int fd_from, fd_to;
+ssize_t bytes_read, bytes_written;
+char buffer[BUFFER_SIZE];
+if (argc != 3)
+{
+(void)dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+(void)dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
+S_IWUSR | S_IRGRP | S_IROTH);
+if (fd_to == -1)
+{
+(void)dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+{
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+break;
+}
+if (bytes_read == -1)
+{
+(void)dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+if (close(fd_from) == -1 || close(fd_to) == -1)
+{
+(void)dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+exit(100);
+}
+return (0);
 }
