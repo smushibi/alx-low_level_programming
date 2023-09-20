f0ffc622801f63e50e82f2ce0b7c26c0f0c14a05
smushibi <samutachika@gmail.com>
2023-09-20 15:24:39 -0700

    displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,24 @@
+ * display_elf_header - Displays information contained in the ELF header
+ * @filename: Path of the ELF file
+ *
+ * Description:
+ * This function opens the specified ELF file, reads its header, and displays
+ * relevant information including magic number, class, data format, version,
+ * OS/ABI, ABI version, type, and entry point address.
+ *
+ * If there is an error in opening, reading, or if the file is not a valid ELF
+ * file, it prints an error message and exits with status code 98.
+ *
+ * Format: the same as readelf -h (version 2.26.1)
+ * If the file is not an ELF file, or on error, exit with status code 98 and
+ * display a comprehensive error message to stderr.
+ *
+ * You are allowed to use lseek once.
+ * You are allowed to use read a maximum of 2 times at runtime.
+ * You are allowed to have as many functions as you want in your source file.
+ * You are allowed to use printf.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -6,81 +27,45 @@
 #include <string.h>
 #include "main.h"
 
-/**
-* display_error - Displays an error message and exits
-* @message: The error message to display
-*
-* Description:
-* This function prints an error message to stderr and exits the program with
-* status code 98.
-*/
-void display_error(char *message)
+void display_elf_header(char *filename)
 {
-fprintf(stderr, "Error: %s\n", message);
-exit(98);
-}
-/**
-* display_magic_number - Displays the magic number of the ELF header
-* @ident: The array containing the magic number
-*
-* Description:
-* This function prints the magic number in hexadecimal format.
-*/
-void display_magic_number(unsigned char *ident)
-{
-printf("Magic:   ");
-for (int i = 0; i < 16; i++)
-{
-printf("%02x ", ident[i]);
-}
-printf("\n");
-}
-/**
-* display_elf_header_info - Displays information from the ELF header
-* @header: Pointer to the ELF header structure
-*
-* Description:
-* This function prints information like class, data format, version, etc.
-* from the ELF header.
-*/
-void display_elf_header_info(Elf32_Ehdr *header)
-{
-printf("Class:   %s\n", (header->e_ident[4] == 1) ? "ELF32" : "ELF64");
-printf("Data:    %s\n", (header->e_ident[5] == 1) ?
-	"2's complement, little endian" : "2's complement, big endian");
-printf("Version: %u\n", header->e_version);
-printf("OS/ABI:  %u\n", header->e_ident[7]);
-printf("ABI Version: %u\n", header->e_ident[8]);
-printf("Type:    %u\n", header->e_type);
-printf("Entry point address: 0x%lx\n", (unsigned long)header->e_entry);
-}
-/**
-* read_elf_header - Reads and validates the ELF header from a file
-* @filename: Path of the ELF file
-* @header: Pointer to the ELF header structure
-*
-* Description:
-* This function opens the specified ELF file, reads its header, and performs
-* necessary validations. If any errors occur, it displays an error message and
-* exits with status code 98.
-*/
-void read_elf_header(char *filename, Elf32_Ehdr *header)
-{
-int fd = open(filename, O_RDONLY);
+int fd;
+
+Elf32_Ehdr header;
+int i;
+
+fd = open(filename, O_RDONLY);
 if (fd == -1)
 {
-display_error("Failed to open file");
+fprintf(stderr, "Error opening file '%s'\n", filename);
+exit(98);
 }
-if (read(fd, header, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr))
+if (read(fd, &header, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr))
 {
+fprintf(stderr, "Error reading ELF header\n");
 close(fd);
-display_error("Failed to read ELF header");
+exit(98);
 }
-if (memcmp(header->e_ident, "\177ELF", 4) != 0)
+if (memcmp(header.e_ident, "\177ELF", 4) != 0)
 {
+fprintf(stderr, "Not an ELF file: '%s'\n", filename);
 close(fd);
-display_error("Not an ELF file");
+exit(98);
 }
+printf("Magic:   ");
+for (i = 0; i < 16; i++)
+{
+printf("%02x ", header.e_ident[i]);
+}
+printf("\n");
+printf("Class:   %s\n", (header.e_ident[4] == 1) ? "ELF32" : "ELF64");
+printf("Data:    %s\n", (header.e_ident[5] == 1) ?
+	"2's complement, little endian" : "2's complement, big endian");
+printf("Version: %u\n", header.e_version);
+printf("OS/ABI:  %u\n", header.e_ident[7]);
+printf("ABI Version: %u\n", header.e_ident[8]);
+printf("Type:    %u\n", header.e_type);
+printf("Entry point address: 0x%lx\n", (unsigned long)header.e_entry);
 close(fd);
 }
 /**
@@ -104,15 +89,13 @@ close(fd);
  */
 int main(int argc, char *argv[])
 {
+char *filename;
 if (argc != 2)
 {
 fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
 return (1);
 }
-char *filename = argv[1];
-Elf32_Ehdr header;
-read_elf_header(filename, &header);
-display_magic_number(header.e_ident);
-display_elf_header_info(&header);
+filename = argv[1];
+display_elf_header(filename);
 return (0);
 }
