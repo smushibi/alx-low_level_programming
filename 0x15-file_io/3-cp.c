b4e2e67cb88e69686a10418a863f83230a8db810
smushibi <samutachika@gmail.com>
2023-10-02 14:27:08 -0700

    3cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <stdlib.h>
 
 #define BUFFER_SIZE 1024
 
 /**
-* error_exit - Prints an error message and exits the program with a specified code.
-*
-* @code: The exit code.
-* @message: The error message to print.
-*/
-void error_exit(int code, const char *message)
+ * print_error - Prints an error message to stderr.
+ * @message: The error message to be printed.
+ */
+void print_error(const char *message)
 {
 dprintf(STDERR_FILENO, "Error: %s\n", message);
-exit(code);
 }
-
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 on success, or an exit status on failure.
+ */
 int main(int argc, char *argv[])
 {
 int fd_from, fd_to;
-const char *file_from, *file_to;
-char buffer[BUFFER_SIZE];
 ssize_t bytes_read, bytes_written;
-
+char buffer[BUFFER_SIZE];
 if (argc != 3)
 {
-error_exit(97, "Usage: cp file_from file_to");
+print_error("Usage: cp file_from file_to");
+exit(97);
 }
-
-file_from = argv[1];
-file_to = argv[2];
-
-fd_from = open(file_from, O_RDONLY);
+fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
 {
-error_exit(98, "Can't read from file");
+print_error("Can't read from file");
+exit(98);
 }
-
-fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
-| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 if (fd_to == -1)
 {
-close(fd_from);
-error_exit(99, "Can't write to file");
+print_error("Can't write to file");
+exit(99);
 }
-
 while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
 bytes_written = write(fd_to, buffer, bytes_read);
 if (bytes_written == -1)
 {
-close(fd_from);
-close(fd_to);
-error_exit(99, "Can't write to file");
+print_error("Can't write to file");
+exit(99);
 }
 }
-
 if (bytes_read == -1)
 {
-close(fd_from);
-close(fd_to);
-error_exit(98, "Can't read from file");
+print_error("Can't read from file");
+exit(98);
 }
 
-if (close(fd_from) == -1 || close(fd_to) == -1)
+
+if (close(fd_from) == -1)
 {
-error_exit(100, "Can't close file descriptor");
+print_error("Can't close fd");
+exit(100);
+}
+if (close(fd_to) == -1)
+{
+print_error("Can't close fd");
+exit(100);
 }
 
 return 0;
