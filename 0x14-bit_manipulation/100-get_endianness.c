22bb152dbd1383d781dc0db968e23e674dd3f719
smushibi <samutachika@gmail.com>
2023-10-26 09:41:13 -0700

    returns the number of bits you would need to flip to get from one number to another.

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,12 @@
 /**
- * get_endianness - Checks the endianness of the system.
- *
- * Return: 0 if big endian, 1 if little endian.
- */
+* get_endianness - Checks the endianness of the system.
+*
+* Return: 0 if big endian, 1 if little endian.
+*/
 int get_endianness(void)
 {
-    unsigned int num = 1;
-    char *byte = (char *)&num;
+unsigned int num = 1;
+char *byte = (char *)&num;
 
-    return (*byte);
+return (*byte);
 }
-
