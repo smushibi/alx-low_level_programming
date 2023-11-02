3295fc0f9697bd429c01339094398bd801320399
smushibi <samutachika@gmail.com>
2023-11-02 05:42:54 -0700

    that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <stddef.h>
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
+
+
+/**
+* create_file - Creates a file and writes text content to it.
+* @filename: The name of the file to create.
+* @text_content: The text content to write to the file.
+*
+* Return: 1 on success, -1 on failure.
+*/
+int create_file(const char *filename, char *text_content)
+{
+int fd, bytes_written;
+
+if (filename == NULL)
+return (-1);
+
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+
+if (text_content != NULL)
+{
+bytes_written = write(fd, text_content, strlen(text_content));
+if (bytes_written == -1)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
+
+
